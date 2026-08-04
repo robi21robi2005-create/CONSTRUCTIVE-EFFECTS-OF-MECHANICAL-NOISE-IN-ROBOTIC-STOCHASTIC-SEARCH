3fe7e626cf068182f83c06ecb02bf73dff748dd6
robi21robi2005-create <robi21robi2005@gmail.com>
2026-08-04 13:15:38 +0300

    Enhance random.h with function documentation
    Added comments to function declarations for clarity.

--- random.h
+++ random.h
@@ -1,6 +1,6 @@
 #pragma once
 
 void set_seed(unsigned int seed);
-double random_01();
-double random_angle();
-double random_step(double x);
+double random_01();    // Uniform sampling between [0, 1] for the Boltzmann probability check
+double random_angle(); // Uniform angular sampling for spatial exploration
+double random_step(double x); // Uniform radial step sampling up to a characteristic length
