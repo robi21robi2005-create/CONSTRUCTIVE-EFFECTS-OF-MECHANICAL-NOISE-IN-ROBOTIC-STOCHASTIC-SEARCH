6c74670d827f0ea1678c9091586807fc6cc070b7
robi21robi2005-create <robi21robi2005@gmail.com>
2026-08-04 13:15:59 +0300

    Enhance random.cpp with function documentation
    Added comments to explain the purpose of functions and the random number generator.

--- random.cpp
+++ random.cpp
@@ -2,6 +2,8 @@
 #include <random>
 #include <cmath>
 
+// Mersenne Twister engine. Highly reliable for generating unbiased Markov chains
+// and preventing unwanted statistical correlations in long simulations.
 std::mt19937 gen;
 
 void set_seed(unsigned int seed)
@@ -9,21 +11,24 @@ void set_seed(unsigned int seed)
     gen.seed(seed);
 }
 
+// Generates a uniform float in [0, 1) used to test the Metropolis Boltzmann factor
 double random_01()
 {
     static std::uniform_real_distribution<double> dist(0.0, 1.0);
     return dist(gen);
 }
 
+// Generates an isotropic scattering angle [0, 2*pi) for the unbiased 2D random walk
 double random_angle()
 {
     static std::uniform_real_distribution<double> dist(0.0, 2.0 * M_PI);
     return dist(gen);
 }
 
+// Generates a random radial displacement in [0, x], controlling the spatial mobility 
+// (used both for regular exploration steps d_max and the mechanical noise epsilon)
 double random_step(double x)
 {
     std::uniform_real_distribution<double> dist(0.0, x);
     return dist(gen);
-
 }
